22db35e56235dbc07ed88bf27140376944ec9d10
DevByEagle <142937112+DevByEagle@users.noreply.github.com>
2025-06-30 03:28:44 +0000

    feat: Refactor Display and Game classes for improved structure and functionality

--- src/main/java/org/gava/Display.java
+++ src/main/java/org/gava/Display.java
@@ -6,10 +6,34 @@ import java.awt.image.BufferStrategy;
 
 // TODO: Make this not public to everyone.
 public class Display extends Canvas {
-    // private static Display instance;
     private JFrame frame;
 
-    protected Display() {
-        
+    protected Display(int width, int height, String title) {
+        frame = new JFrame(title);
+        frame.setSize(width, height);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(this, BorderLayout.CENTER);
+        frame.setVisible(true);
+        setFocusable(true);
+    }
+
+    public JFrame getFrame() {
+        return frame;
+    }
+
+    @Override
+    public Graphics getGraphics() {
+        BufferStrategy bs = getBufferStrategy();
+        return bs.getDrawGraphics();
+    }
+
+    public void createBuffer() {
+        this.createBufferStrategy(2);
+    }
+
+    public void swapBuffers() {
+        BufferStrategy bs = getBufferStrategy();
+        bs.show();
     }
 }
--- src/main/java/org/gava/Game.java
+++ src/main/java/org/gava/Game.java
@@ -1,28 +1,51 @@
 package org.gava;
 
+import java.awt.Graphics;
+
 import org.gava.utils.Disposable;
 
 public class Game implements Disposable {
-    private boolean running = true;
+    private final Display display;
+
+    private String title = "Gava2D";
+    private boolean running = false;
+
     private final int fps = 60;
 
     public Game() {
+        display = new Display(800, 600, title);
     }
 
     public void dispose() {
-
     }
-
-    //    public void exit() {}
 
     public final void run() {
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1_000_000_000.0 / fps;
+        // double delta = 0;
 
+        display.createBuffer();
+
+        while (running) {
+            long now = System.nanoTime();
+            if ((now - lastTime) >= nsPerTick) {
+                update();
+                draw(display.getGraphics());
+                lastTime = now;
+            }
+        }
     }
 
-    protected void draw() {
+    protected void draw(Graphics g) {
+        display.swapBuffers();
     }
 
-    protected void update() {
+    protected void update() {}
+
+    public void setTitle(String title) {
+        this.title = title;
+        if (display.getFrame() != null) {
+            display.getFrame().setTitle(title);
+        }
     }
-    
 }
